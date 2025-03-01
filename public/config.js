ce4168180018b32a4f15b05e2b5c46fe222a6d94
bbhsvip <bbhsvip@hotmail.com>
2025-03-01 17:37:14 +0800

    Add files via upload

---
+++ public/config.js
@@ -0,0 +1,35 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'wwqq网络空间监控',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur2848432-31a285b48d16eb00e84e37ad',
+  ],
+
+  // 日志天数
+  CountDays: 30,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 背景图片
+  Image: 'https://ps.ssl.qhmsg.com/t0200ce14e175af6dcf.jpg',
+
+  // 背景颜色
+  color: '#555555',
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '主页',
+      url: 'https://wwqq.us.kg'
+    },
+    {
+      text: 'Github',
+      url: 'https://github.com/bbhssvip/uptime-status-pro'
+    },
+  ],
+};
